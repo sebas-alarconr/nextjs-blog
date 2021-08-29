f65584d4c05760d99d4cd64d356805f48d8f4c39
Sebastian Alarcon <sebas.alarcon8@gmail.com>
2021-08-29 22:41:01 +0200

    Fixing type issue

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -2,7 +2,7 @@ import { GetStaticProps, GetStaticPaths } from "next";
 import Head from "next/head";
 
 import { getAllPostIds, getPostData } from "../../lib/posts";
-import { Post } from "../../types/posts";
+import { Post as PostType } from "../../types/posts";
 
 import Layout from "../../components/layout";
 import Date from "../../components/date";
@@ -10,7 +10,7 @@ import Date from "../../components/date";
 import utilStyles from "../../styles/utils.module.css";
 
 type PostProps = {
-  postData: Post & {
+  postData: PostType & {
     contentHtml: string;
   };
 };
